ddc5b06ac40040af654089f13c302d0f4dab23c4
neripark <neripark@gmail.com>
2025-03-01 19:09:52 +0900

    fix reference error of vitest api

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,10 @@
 import '@testing-library/jest-dom';
 import { server } from '../mocks/server';
+import { expect } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
+
+// @testing-library/jest-domのマッチャーをVitestに追加
+expect.extend(matchers);
 
 // MSWのサーバーを起動
 beforeAll(() => server.listen());
