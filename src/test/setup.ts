0d1c95e50c9b727ce9478fd6d1dbaf7d9b1c8ff2
neripark <neripark@gmail.com>
2025-03-01 19:14:53 +0900

    fix test file

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,10 +1,5 @@
 import '@testing-library/jest-dom';
 import { server } from '../mocks/server';
-import { expect } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
-
-// @testing-library/jest-domのマッチャーをVitestに追加
-expect.extend(matchers);
 
 // MSWのサーバーを起動
 beforeAll(() => server.listen());
